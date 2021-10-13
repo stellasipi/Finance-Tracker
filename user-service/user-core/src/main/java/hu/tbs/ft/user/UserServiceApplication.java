6025f6ce246986fa5caf62da86a0e5280c0c28de
Stella Tóth-Baranyi <stellasipi@gmail.com>
2021-10-14 00:39:16 +0200

    Add swagger config

--- user-service/user-core/src/main/java/hu/tbs/ft/user/UserServiceApplication.java
+++ user-service/user-core/src/main/java/hu/tbs/ft/user/UserServiceApplication.java
@@ -1,10 +1,14 @@
 package hu.tbs.ft.user;
 
+import hu.tbs.ft.common.configuration.apidocs.ApiDocsConfiguration;
+import hu.tbs.ft.common.configuration.apidocs.ResourceServerWebSecurityConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ApiDocsConfiguration.class, ResourceServerWebSecurityConfig.class})
 @Slf4j
 public class UserServiceApplication {
     public static void main(String[] args) {
