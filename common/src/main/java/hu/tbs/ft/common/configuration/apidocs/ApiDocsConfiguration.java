2f3608ac4cc593c40513c3ccbfd12ec2f04e402f
Stella Tóth-Baranyi <stellasipi@gmail.com>
2021-10-14 01:21:56 +0200

    Working swagger ui

--- common/src/main/java/hu/tbs/ft/common/configuration/apidocs/ApiDocsConfiguration.java
+++ common/src/main/java/hu/tbs/ft/common/configuration/apidocs/ApiDocsConfiguration.java
@@ -6,17 +6,15 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class ApiDocsConfiguration {
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("hu.tbs.ft"))
                 .paths(PathSelectors.any())
                 .build();
     }
