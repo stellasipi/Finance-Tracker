bf3851401e9f229ef532f42257a1e6c3e856bbb1
Stella Tóth-Baranyi <stellasipi@gmail.com>
2022-12-12 22:52:23 +0100

    Add screenshots

--- api-gateway/src/main/java/hu/tbs/ft/apigateway/config/GatewayWebSecurityConfig.java
+++ api-gateway/src/main/java/hu/tbs/ft/apigateway/config/GatewayWebSecurityConfig.java
@@ -1,8 +1,15 @@
 package hu.tbs.ft.apigateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 public class GatewayWebSecurityConfig extends WebSecurityConfigurerAdapter {
